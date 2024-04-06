25ef84525f73409796c739b36c838e69cc2e40fe
takumakoike <takuma.151e@gmail.com>
2024-04-06 14:55:40 +0900

    deploy

--- next-ortho/src/components/Heading.js
+++ next-ortho/src/components/Heading.js
@@ -4,7 +4,7 @@ export default function Heading(props){
     return(
         <div>
             <div className="container mx-auto mt-10">
-                <h1 className="text-3xl font-bold text-center">これは{props.title}です</h1>
+                <h1 className="text-3xl font-bold text-center">これは{props.title}です!</h1>
                 <div className="flex w-full justify-evenly mt-10">
                     <Link href="/">
                         <p>トップページ</p>

ac05de982ec9b2deaeadd83960c7a508fe958cff
takumakoike <takuma.151e@gmail.com>
2024-04-06 15:12:24 +0900

    about

---
+++ next-ortho/src/app/fees/about/page.tsx
@@ -0,0 +1,10 @@
+import Heading from "@/components/Heading"
+
+
+export default function FeesAbout() {
+    return (
+        <div>
+            <Heading title="これはどうなる" />
+        </div>
+    );
+}
--- next-ortho/src/app/page.tsx
+++ next-ortho/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Heading from "../components/Heading"
 
 

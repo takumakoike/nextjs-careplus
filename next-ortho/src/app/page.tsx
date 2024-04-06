f882806b19ff9e9cb22f35d6022a291815d850c2
takumakoike <takuma.151e@gmail.com>
2024-04-06 18:50:31 +0900

    lower

--- next-ortho/src/app/page.tsx
+++ next-ortho/src/app/page.tsx
@@ -1,10 +1,19 @@
-import Heading from "../components/Heading"
-
+import Heading from "@/components/Heading"
+import {Navigation} from "@/components/Navigation"
 
 export default function Home() {
   return (
-      <div>
-        <Heading title="トップページ" />
-      </div>
+      <>
+        <main className="w-full flex flex-col md:flex-row">
+            <Navigation />
+            <div className="main-contents bg-white w-full md:w-5/6">
+                <h2 className="text-2xl font-bold p-3">本日の予定</h2>
+                <div className="place__information p-3">
+                    <span className="mr-1 md:mr-10">業務施設</span>
+                    <span className="border border-gray-400 bg-gray-300 p-1 md:p-2 inline-block w-3/5 md:w-2/5 text-sm md:text-md">サンプル施設A</span>
+                </div>
+            </div>
+        </main>
+      </>
   );
 }
--- next-ortho/src/components/Heading.js
+++ next-ortho/src/components/Heading.js
@@ -1,28 +1,35 @@
+
+import Head from "next/head"
 import Link from "next/link"
 
 export default function Heading(props){
     return(
-        <div>
-            <div className="container mx-auto mt-10">
-                <h1 className="text-3xl font-bold text-center">これは{props.title}です</h1>
-                <div className="flex w-full justify-evenly mt-10">
-                    <Link href="/">
-                        <p>トップページ</p>
-                    </Link>
-                    <Link href="/patients">
-                        <p>患者</p>
-                    </Link>
-                    <Link href="/staff">
-                        <p>スタッフ</p>
-                    </Link>
-                    <Link href="/places">
-                        <p>施設</p>
-                    </Link>
-                    <Link href="/fees">
-                        <p>料金</p>
-                    </Link>
+        <>
+            <Head>
+                <title>{props.title}っです</title>
+            </Head>
+            <header>
+                <div className="container mx-auto mt-10">
+                    <h1 className="text-3xl font-bold text-center">情報管理サイト</h1>
+                    <div className="flex w-full justify-evenly mt-6">
+                        <Link href="/">
+                            <p>トップページ</p>
+                        </Link>
+                        <Link href="/patients">
+                            <p>患者</p>
+                        </Link>
+                        <Link href="/staff">
+                            <p>スタッフ</p>
+                        </Link>
+                        <Link href="/places">
+                            <p>施設</p>
+                        </Link>
+                        <Link href="/fees">
+                            <p>料金</p>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </header>
+        </>
     )
 }
